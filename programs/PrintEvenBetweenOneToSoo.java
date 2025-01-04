fa6dcdaf1564a7ec70b15c53fc2efa99a202cec6
arpitparekh <arpitparekh9@gmail.com>
2025-01-04 19:00:42 +0530

    loops

--- programs/PrintEvenBetweenOneToSoo.java
+++ programs/PrintEvenBetweenOneToSoo.java
@@ -10,5 +10,18 @@ public class PrintEvenBetweenOneToSoo {
       }
     }
 
+    int sum = 0;  // 1  // 3  // 6
+
+    for (int i = 1; i <= 100; i++) { // i 1,2,3,4,5
+      sum = sum + i;
+    }
+
+    System.out.println(sum);
+
+    /*
+     * Write a program to find the sum of all even numbers between 1 to n.
+     * Write a program to find the sum of all odd numbers between 1 to n.
+     */
+
   }
 }
